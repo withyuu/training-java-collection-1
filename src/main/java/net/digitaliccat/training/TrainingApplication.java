13345298d05f5bd2518c03c060ab4e9b2d7cc542
Punaek Chayuti <punaek.cha@ascendcorp.com>
2017-06-14 11:07:01 +0700

    Placeholders

--- src/main/java/net/digitaliccat/training/TrainingApplication.java
+++ src/main/java/net/digitaliccat/training/TrainingApplication.java
@@ -1,19 +1,9 @@
 package net.digitaliccat.training;
 
-import net.digitaliccat.training.list.suicide.RecursiveSuicideSolver;
-import net.digitaliccat.training.list.suicide.SuicideSolverInterface;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.util.StopWatch;
-import sun.tools.jar.CommandLine;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class TrainingApplication {
@@ -22,34 +12,10 @@ public class TrainingApplication {
 		SpringApplication.run(TrainingApplication.class, args);
 	}
 
-	@Autowired
-	@Qualifier("linkedListSolver")
-    SuicideSolverInterface linkedListSolver;
-
-    @Autowired
-    @Qualifier("recursiveSolver")
-    SuicideSolverInterface recursiveSolver;
-
-	@Bean
-	CommandLineRunner executeSuicideProblemWithLinkedList() {
-		return args -> {
-            StopWatch timer = new StopWatch();
-            timer.start();
-            int result = linkedListSolver.solve(999, 3, 17);
-            timer.stop();
-            System.out.printf("[LinkedListSolver] Result:%d, Time Used:%d%n", result, timer.getTotalTimeMillis());
-
-		};
-	}
-
     @Bean
-    CommandLineRunner executeSuicideProblemWithRecursive() {
+    CommandLineRunner run() {
         return args -> {
-            StopWatch timer = new StopWatch();
-            timer.start();
-            int result = recursiveSolver.solve(999, 3, 17);
-            timer.stop();
-            System.out.printf("[RecursiveSolver] Result:%d, Time Used:%d%n", result, timer.getTotalTimeMillis());
+            // TODO implementation goes here
         };
     }
 
--- src/main/java/net/digitaliccat/training/list/MyOwnList.java
+++ src/main/java/net/digitaliccat/training/list/MyOwnList.java
@@ -1,82 +1,34 @@
 package net.digitaliccat.training.list;
 
 
-import java.util.Arrays;
 
 public class MyOwnList {
 
-    private int size;
-
-    private Object[] collection;
-
     public MyOwnList() {
-        size = 0;
-        collection = new Object[10];
+        // TODO implementation goes here
     }
 
     public void add(Object obj) {
-        ensureCapacity();
-
-        collection[size] = obj;
-        ++size;
+        // TODO implementation goes here
     }
 
     public void add(int index, Object obj) {
-        checkRangeForAdd(index);
-
-        ensureCapacity();
-        System.arraycopy(collection, index, collection, index + 1, size - index);
-        collection[index] = obj;
-        ++size;
+        // TODO implementation goes here
     }
 
     public Object remove(int index) {
-        checkRange(index);
-
-        Object ret = collection[index];
-        int numMoved = size - index - 1;
-        if (numMoved > 0)
-            System.arraycopy(collection, index + 1, collection, index,
-                    numMoved);
-
-        // clear item
-        collection[--size] = null;
-
-        return ret;
+        // TODO implementation goes here
+        return null;
     }
 
     public Object get(int index) {
-        checkRange(index);
-        return collection[index];
+        // TODO implementation goes here
+        return null;
     }
 
     public int size() {
-        return size;
-    }
-
-    private void ensureCapacity() {
-        if (size + 1 > collection.length) {
-            increaseSize();
-        }
-    }
-
-    private void increaseSize() {
-        collection = Arrays.copyOf(collection, collection.length * 2);
-    }
-
-    private void checkRange(int index) {
-        if (index >= size) {
-            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
-        }
-    }
-
-    private void checkRangeForAdd(int index) {
-        if (index > size || index < 0)
-            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
-    }
-
-    private String outOfBoundsMsg(int index) {
-        return String.format("Index %d out of bound for the list of size %d", index, size);
+        // TODO implementation goes here
+        return -1;
     }
 
 }
